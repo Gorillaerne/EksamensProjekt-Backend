ecf30253319f50f0aa104c275dd7b266b901913c
Nilket <joni0010@stud.kea.dk>
2025-12-08 12:19:11 +0100

    skal lige pull

--- src/main/java/gustavo/com/eksamenprojektbackend/Product/DTO/ProductWarehouseDTO.java
+++ src/main/java/gustavo/com/eksamenprojektbackend/Product/DTO/ProductWarehouseDTO.java
@@ -1,6 +1,4 @@
 package gustavo.com.eksamenprojektbackend.Product.DTO;
 
 public record ProductWarehouseDTO () {
-
-    //navn på lageret og antal af vare.
 }
--- src/main/java/gustavo/com/eksamenprojektbackend/Product/Service/ProductService.java
+++ src/main/java/gustavo/com/eksamenprojektbackend/Product/Service/ProductService.java
@@ -15,24 +15,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductService {
 
-    private final IProductRepository productRepostiory;
+    private final IProductRepository productRepository;
     private final LogService logService;
     private final IWarehouseProductRepository warehouseProductRepository;
     private final IWarehouseRepository warehouseRepository;
 
 
-    public ProductService(IProductRepository productRepostiory, LogService logService, IWarehouseProductRepository warehouseProductRepository, IWarehouseRepository warehouseRepository) {
-        this.productRepostiory = productRepostiory;
+    public ProductService(IProductRepository productRepository, LogService logService, IWarehouseProductRepository warehouseProductRepository, IWarehouseRepository warehouseRepository) {
+        this.productRepository = productRepository;
         this.logService = logService;
         this.warehouseProductRepository = warehouseProductRepository;
         this.warehouseRepository = warehouseRepository;
@@ -41,7 +37,7 @@ public class ProductService {
     public ProductDTO createProduct(ProductDTO product, User user) {
         try {
 
-            Product createdProduct = productRepostiory.save(new Product(product.name(),product.description(),product.picture(),product.SKU(),product.price(),new ArrayList<>()));
+            Product createdProduct = productRepository.save(new Product(product.name(),product.description(),product.picture(),product.SKU(),product.price(),new ArrayList<>()));
             logService.createLogFromProduct(createdProduct, user, "User: " + user.getUsername() +" | Har oprettet et nyt produkt: " + createdProduct.getName());
             return new ProductDTO(createdProduct.getId(),createdProduct.getName(), createdProduct.getDescription(), createdProduct.getPicture(), createdProduct.getSKU(), createdProduct.getPrice());
 
@@ -51,11 +47,11 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts(){
-        return productRepostiory.findAll();
+        return productRepository.findAll();
     }
 
     public List<SearchBarProductDTO> getAllProductsForSearchBar(){
-        List<Product> productList = productRepostiory.findAll();
+        List<Product> productList = productRepository.findAll();
 
         List<SearchBarProductDTO> dtoList = productList.stream()
                 .map(product -> new SearchBarProductDTO(
@@ -92,7 +88,7 @@ public class ProductService {
                 warehouseProduct.setWarehouse(warehouseRepository.findById(dto.warehouseId()).orElseThrow(()->
                         new ResponseStatusException(HttpStatus.BAD_REQUEST, "Kunne ikke opdatere varehus id")
                 ));
-                warehouseProduct.setProduct(productRepostiory.findById(dto.productId()).orElseThrow(()->
+                warehouseProduct.setProduct(productRepository.findById(dto.productId()).orElseThrow(()->
                         new ResponseStatusException(HttpStatus.BAD_REQUEST, "Kunne ikke opdatere produkt id")
                         ));
             }
@@ -107,7 +103,7 @@ public class ProductService {
     }
 
     public Product updateProduct(Integer id, Product productRequest, User user) {
-        Product product = productRepostiory.findById(id)
+        Product product = productRepository.findById(id)
                 .orElseThrow(()-> new RuntimeException("Produktet med dette id kunne ikke findes " + id));
 
         StringBuilder changes = new StringBuilder();
@@ -145,7 +141,7 @@ public class ProductService {
             changes.append("SKU ændret fra ").append(product.getSKU()).append(" til ").append(productRequest.getSKU()).append(". ");
         }
 
-        Product productResponse = productRepostiory.save(product);
+        Product productResponse = productRepository.save(product);
 
         if (!changes.isEmpty()) {
             logService.createLogFromProduct(product, user, "User: " + user.getUsername() + "| ændrede producktet. Ændringer: " + changes);
@@ -156,14 +152,14 @@ public class ProductService {
 
 
     public Product getProductById(Integer id) {
-        return productRepostiory.findById(id)
+        return productRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "Produkt ikke fundet"));
     }
 
     public List<ProductDTO> getAllProductsDto() {
         List<ProductDTO> productDTOList = new ArrayList<>();
-        List<Product> productList = productRepostiory.findAll();
+        List<Product> productList = productRepository.findAll();
         for (Product product : productList){
             int quanity = 0;
             for (WarehouseProduct warehouseProduct : product.getWarehouseProductList()){
