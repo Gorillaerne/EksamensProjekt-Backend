7f98ae2e41761604667bee9615dbe1f3ebf4b583
Nilket <joni0010@stud.kea.dk>
2025-12-11 21:23:05 +0100

    skal lige pulle

--- src/main/java/gustavo/com/eksamenprojektbackend/Product/Controllers/ProductController.java
+++ src/main/java/gustavo/com/eksamenprojektbackend/Product/Controllers/ProductController.java
@@ -46,7 +46,6 @@ public class ProductController {
 
     @PostMapping("/delivery")
     public ResponseEntity<?> registerDeliveryOfGoods(Authentication authentication, @RequestBody List<RegisterDeliveryDTO> deliveryDTOS){
-        System.out.println(deliveryDTOS);
         User user = (User) authentication.getPrincipal();
         return new ResponseEntity<>(productService.registerDeliveryOfGoods(deliveryDTOS, user), HttpStatus.CREATED);
     }
@@ -63,7 +62,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productService.getProductWithWarehouseDTO(id));
     }
 
-    
+
 
 
 
